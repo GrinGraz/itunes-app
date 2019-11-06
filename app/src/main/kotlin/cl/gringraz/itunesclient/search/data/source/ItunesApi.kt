27bfd09ee95fe2e423021a49835de54301c18918
Christopher Ruz <christopher.ruz@adevinta.com>
2019-11-06 11:55:36 -0300

    [PR-01] Update retrofit to add coroutines built-in support

---
+++ app/src/main/kotlin/cl/gringraz/itunesclient/search/data/source/ItunesApi.kt
@@ -0,0 +1,29 @@
+package cl.gringraz.itunesclient.search.data.source
+
+import cl.gringraz.itunesclient.search.data.entities.AlbumEntity
+import cl.gringraz.itunesclient.search.data.entities.ArtistEntity
+import cl.gringraz.itunesclient.search.data.entities.SearchResult
+import cl.gringraz.itunesclient.search.data.entities.TrackEntity
+import retrofit2.http.GET
+import retrofit2.http.Query
+
+
+interface ItunesApi {
+    @GET("search")
+    suspend fun searchArtists(
+        @Query("term") terms: String,
+        @Query("entity") entity: String = "musicArtist"
+    ): SearchResult<ArtistEntity>
+
+    @GET("lookup")
+    suspend fun lookupAlbums(
+        @Query("id") artistId: Long,
+        @Query("entity") entity: String = "album"
+    ): SearchResult<AlbumEntity>
+
+    @GET("lookup")
+    suspend fun lookupTracks(
+        @Query("id") albumId: Long,
+        @Query("entity") entity: String = "song"
+    ): SearchResult<TrackEntity>
+}
