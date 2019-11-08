305215fe4e2037c12bc7c9bde75e060ff37284a8
Christopher Ruz <c.ruz.olate@gmail.com>
2019-11-08 02:50:03 -0300

    [PR-07] Add search recycler adapter and item layout

---
+++ app/src/main/kotlin/cl/gringraz/itunesclient/feature/search/SearchAdapter.kt
@@ -0,0 +1,39 @@
+package cl.gringraz.itunesclient.feature.search
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import cl.gringraz.itunesclient.R
+import cl.gringraz.itunesclient.domain.model.Artist
+import cl.gringraz.itunesclient.ui.BaseAdapter
+import cl.gringraz.itunesclient.ui.BaseViewHolder
+
+open class ArtistAdapter(
+    private val manager: AdapterManager
+) : BaseAdapter<Artist>() {
+
+    interface AdapterManager {
+        fun onArtistClicked(item: Artist, position: Int)
+    }
+
+    override fun provideComparator() = compareBy(Artist::artistId)
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): BaseViewHolder<Artist> {
+        val itemView = LayoutInflater.from(parent.context).inflate(R.layout.item_artist, parent, false)
+
+        return ArtistViewHolder(itemView).also {
+
+        }
+    }
+
+    fun isEmpty() = items.isEmpty()
+
+    inner class ArtistViewHolder(
+        itemView: View
+    ) :
+        BaseViewHolder<Artist>(itemView) {
+        override fun bindView(item: Artist) {
+
+        }
+    }
+}
